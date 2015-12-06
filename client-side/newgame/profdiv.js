9ffe0cbb29bfa3e24ca55f1f8445d278df4c495d
Patrick Hahn <phahn13@gmail.com>
2015-12-06 14:42:54 -0500

    Commenting out unfinished code so game will work for everyone else in the meantime.

--- client-side/newgame/profdiv.js
+++ client-side/newgame/profdiv.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 
 	$(".professor").click(function(e){
-		$.ajax(url_base + "session.php",
-	         {type: "PUT",
-		              dataType: "json",
-                  data: $(this).serialize(),
-		              success: function(todo_ids, status, jqXHR) {
-		                for (var i=0; i<todo_ids.length; i++) {
-			                oad_todo_item(todo_ids[i]);
-		                }
-		              }
-	         });
+		// $.ajax(url_base + "session.php",
+	  //        {type: "PUT",
+		//               dataType: "json",
+    //               data: $(this).serialize(),
+		//               success: function(todo_ids, status, jqXHR) {
+		//                 for (var i=0; i<todo_ids.length; i++) {
+		// 	                oad_todo_item(todo_ids[i]);
+		//                 }
+		//               }
+	  //        });
 		window.location.href="../currentgame/currentgame.html";
 	});
 
