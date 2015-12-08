cca1a49c74907bc9a22acc32eede16baa7ec6a1b
Patrick Hahn <phahn13@gmail.com>
2015-12-08 18:48:40 -0500

    Started working on loading login info from session variables.

--- server-side/session.php
+++ server-side/session.php
@@ -9,25 +9,60 @@ $path_components = explode('/', $_SERVER['PATH_INFO']);
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
   // return session state
-  header("HTTP/1.0 404 Not Found");
-  print("path components, 1: " . $path_components[1] . " 2: " . $path_components[2] . " 3: " . $path_components[3]);
-  exit();
 
-  // update session state
   if ((count($path_components) >= 2) && ($path_components[1] == "login")) {
 
+    $json_obj = array('id' => $SESSION_['userId'],
+		      'username' => $SESSION_['username'],
+		      'password' => $SESSION_['password']);
+
+    header("Content-type: application/json");
+    print(json_encode($json_obj));
+    exit();
   }
-} else if ($_SERVER['REQUEST_METHOD'] == "PUT") {
-  header("HTTP/1.0 404 Not Found");
-  print("path components, 1: " . $path_components[1] . " 2: " . $path_components[2] . " 3: " . $path_components[3]);
-  exit();
+  else if ((count($path_components) >= 2) && ($path_components[1] == "ai")) {
 
+    $json_obj = array('id' => $SESSION_['aiId'],
+		      'depth' => $SESSION_['depth'],
+		      'type' => $SESSION_['type'],
+          'name' => $SESSION_['aiName']);
+
+    header("Content-type: application/json");
+    print(json_encode($json_obj));
+    exit();
+  }
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
   // update session state
-  if ((count($path_components) >= 2) && ($path_components[1] == "login")) {
 
+  if ((count($path_components) >= 5) && ($path_components[1] == "login")) {
+
+    $SESSION_['userId'] = intval($path_components[2]);
+    $SESSION_['username'] = $path_components[3];
+    $SESSION_['password'] = $path_components[4];
+
+    $json_obj = array('id' => $SESSION_['userId'],
+		      'username' => $SESSION_['username'],
+		      'password' => $SESSION_['password']);
+
+    header("Content-type: application/json");
+    print(json_encode($json_obj));
+    exit();
+  }
+  else if ((count($path_components) >= 6) && ($path_components[1] == "ai")) {
+    $SESSION_['aiId'] = intval($path_components[2]);
+    $SESSION_['aiName'] = $path_components[3];
+    $SESSION_['type'] = intval($path_components[4]);
+    $SESSION_['depth'] = intval($path_components[5]);
+
+    $json_obj = array('id' => $SESSION_['aiId'],
+          'depth' => $SESSION_['depth'],
+          'type' => $SESSION_['type'],
+          'name' => $SESSION_['aiName']);
+
+    header("Content-type: application/json");
+    print(json_encode($json_obj));
+    exit();
   }
 }
-
-// $_SESSION['k1'] = "Here is some information saved in \$_SESSION";
 
 ?>
